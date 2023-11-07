1b9cfcf97d9b27a2aeaa9a7e23372eb049958aac
AKhil Kukadiya <73292704+Akhil1939@users.noreply.github.com>
2023-11-07 15:42:56 +0530

    reactive form example

---
+++ click-to-unlock-workspace/projects/reactive-dynamic-form/src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'reactive-dynamic-form';
+
+  constructor(public fb: FormBuilder) {}
+
+  exampleForm: FormGroup = this.fb.group({});
+
+  get hobbies() {
+    return this.exampleForm.get('hobbyList') as FormArray;
+  }
+
+  addHobby() {
+    const exampleForm = this.fb.group({
+      title: ['', Validators.required],
+      playLevel: ['', Validators.required],
+    });
+
+    this.hobbies.push(exampleForm);
+  }
+
+  deleteHobby(i: number) {
+    this.hobbies.removeAt(i);
+  }
+
+  ngOnInit() {
+    this.exampleForm= this.fb.group({
+      name: [''],
+      surname: [''],
+      hobbyList: this.fb.array([]),
+    });
+    this.exampleForm.valueChanges.subscribe((res) => console.log(res));
+  }
+
+}
